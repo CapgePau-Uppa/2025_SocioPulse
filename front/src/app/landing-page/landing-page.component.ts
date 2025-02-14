b5a7ebbdc8f50f83a92e896d35a2aeca5c315438
rexfollloys <rexfolloys@gmail.com>
2025-02-14 11:45:49 +0100

    V1 map + V1 new UI

--- front/src/app/landing-page/landing-page.component.ts
+++ front/src/app/landing-page/landing-page.component.ts
@@ -1,7 +1,9 @@
-import {Component, inject} from '@angular/core';
+import {Component, OnInit, AfterViewInit} from '@angular/core';
 import {MatButton} from '@angular/material/button';
 import {RouterLink} from '@angular/router';
 import {FeaturedComponent} from '../featured/featured.component';
+import * as L from 'leaflet';
+import { ProjectsService } from '../services/projects.service';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +15,32 @@ import {FeaturedComponent} from '../featured/featured.component';
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements AfterViewInit {
+  private map!: L.Map;
 
+  private initMap(): void {
+    this.map = L.map('map').setView([43.3, -0.3667], 6); // Set initial coordinates and zoom
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(this.map);
+
+    /*
+    L.marker([43.3, -0.3667]).addTo(this.map)
+      .bindPopup('A marker on the map')
+      .openPopup();*/
+    }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+    projects: any[] = [];
+
+    constructor(private projectService: ProjectsService) { }
+
+    ngOnInit(): void {
+      this.projectService.getProjects().subscribe(data => {
+        this.projects = data;
+      });
+    }
 }
