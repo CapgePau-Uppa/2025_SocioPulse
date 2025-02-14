482169f7b39ce76d7e4da27d2b093d7192f55950
rexfollloys <rexfolloys@gmail.com>
2025-02-14 12:47:10 +0100

    projects are shown in the map

--- front/src/app/landing-page/landing-page.component.ts
+++ front/src/app/landing-page/landing-page.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnInit, AfterViewInit} from '@angular/core';
-import {MatButton} from '@angular/material/button';
-import {RouterLink} from '@angular/router';
-import {FeaturedComponent} from '../featured/featured.component';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { FeaturedComponent } from '../featured/featured.component';
 import * as L from 'leaflet';
 import { ProjectsService } from '../services/projects.service';
+import 'leaflet/dist/leaflet.css';
 
 @Component({
   selector: 'app-root',
@@ -15,8 +16,9 @@ import { ProjectsService } from '../services/projects.service';
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
-export class LandingPageComponent implements AfterViewInit {
+export class LandingPageComponent implements OnInit {
   private map!: L.Map;
+  projects: any[] = [];
 
   private initMap(): void {
     this.map = L.map('map').setView([43.3, -0.3667], 6); // Set initial coordinates and zoom
@@ -25,23 +27,20 @@ export class LandingPageComponent implements AfterViewInit {
       attribution: '© OpenStreetMap contributors'
     }).addTo(this.map);
 
-    /*
-    L.marker([43.3, -0.3667]).addTo(this.map)
-      .bindPopup('A marker on the map')
-      .openPopup();*/
+    for (let i = 0; i < this.projects.length; i++) {
+      L.marker([this.projects[i].latitude, this.projects[i].longitude]).addTo(this.map)
+        .bindPopup(this.projects[i].name)
+        .openPopup();
     }
-
-  ngAfterViewInit(): void {
-    this.initMap();
   }
-    projects: any[] = [];
 
-    constructor(private projectService: ProjectsService) { }
+  ngOnInit(): void {
+    console.log(sessionStorage.getItem('token'));
+    this.projectService.getProjects().subscribe(data => {
+      this.projects = data;
+      this.initMap(); // Initialize the map after projects are loaded
+    });
+  }
 
-    ngOnInit(): void {
-      console.log(sessionStorage.getItem('token'));
-      this.projectService.getProjects().subscribe(data => {
-        this.projects = data;
-      });
-    }
+  constructor(private projectService: ProjectsService) { }
 }
