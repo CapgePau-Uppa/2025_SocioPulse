f31ea05e0a569cccf9be46b433098fc29b70763d
rexfollloys <rexfolloys@gmail.com>
2025-02-20 11:21:41 +0100

    Print project in map

--- front/src/app/map/map.component.ts
+++ front/src/app/map/map.component.ts
@@ -1,5 +1,7 @@
 import { Component, AfterViewInit } from '@angular/core';
+import { ProjectsService } from '../services/projects.service';
 import * as L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -7,25 +9,27 @@ import * as L from 'leaflet';
 })
 export class MapComponent implements AfterViewInit {
   private map!: L.Map;
+  projects: any[] = [];
 
   private initMap(): void {
-    this.map = L.map('map', {
-      center: [ 39.8282, -98.5795 ],
-      zoom: 3
-    });
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      minZoom: 3,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
+    this.map = L.map('map').setView([43.3, -0.3667], 6); // Set initial coordinates and zoom
 
-    tiles.addTo(this.map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(this.map);
+
+    for (let i = 0; i < this.projects.length; i++) {
+      L.marker([this.projects[i].latitude, this.projects[i].longitude]).addTo(this.map)
+        .bindPopup(this.projects[i].name)
+        .openPopup();
+    }
   }
-
-  constructor() { }
+  constructor(private projectService: ProjectsService) { }
 
   ngAfterViewInit(): void {
-    this.initMap();
-
+    this.projectService.getProjects().subscribe((data: any[]) => {
+      this.projects = data;
+      this.initMap(); // Initialize the map after projects are loaded
+    });
   }
 }
