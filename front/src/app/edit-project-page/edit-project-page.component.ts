5296d1ddc30b8937d2db269e60fe324c9ce8efb5
rexfollloys <rexfolloys@gmail.com>
2025-04-02 18:58:56 +0200

    patch attemps for map bug in project details. keep me updated if you have the issue plz

---
+++ front/src/app/edit-project-page/edit-project-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-project-page',
+  imports: [],
+  templateUrl: './edit-project-page.component.html',
+  styleUrl: './edit-project-page.component.scss'
+})
+export class EditProjectPageComponent {
+
+}
--- front/src/app/project-detail-page/project-detail-page.component.ts
+++ front/src/app/project-detail-page/project-detail-page.component.ts
@@ -24,7 +24,8 @@ import { RendezVousModalComponent } from '../rendez-vous-modal/rendez-vous-modal
 export class ProjectDetailPageComponent implements AfterViewInit {
   project: any;
   requestStatus: string = 'none';
-  canAccess: boolean = false; // Stocke le résultat du test d'accès
+  canAccess: boolean = false;
+  isMapLoaded: boolean = false;
   private map!: L.Map;
 
   private initMap(): void {
@@ -42,7 +43,13 @@ export class ProjectDetailPageComponent implements AfterViewInit {
     L.marker([this.project.latitude, this.project.longitude]).addTo(this.map)
       .bindPopup(this.project.name)
       .openPopup();
+
+    // Attendre que la carte soit complètement chargée
+    this.map.whenReady(() => {
+      this.isMapLoaded = true;
+    });
   }
+
 
   constructor(
     private route: ActivatedRoute,
@@ -182,7 +189,7 @@ public checkUserAccess(): void {
       data: { projectId: this.project.id }
     });
   }
-  
+
   openDialog(): void {
     alert('Fonctionnalité non implémentée');
   }
