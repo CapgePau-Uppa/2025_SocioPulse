bda4e4f3127d3328f5b4f1cb893c7e7cd185628c
rexfollloys <79576518+rexfollloys@users.noreply.github.com>
2025-04-08 19:40:41 +0200

    Update Report.php

--- back/app/Models/Report.php
+++ back/app/Models/Report.php
@@ -6,9 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Report extends Model
 {
-    protected $fillable = ['name', 'path', 'project_id'];
+    protected $fillable = [
+        'name',
+        'path',
+        'project_id',
+        'category_id', // Ajout ici pour pouvoir utiliser create() avec cette valeur
+    ];
+
     public function project()
     {
         return $this->belongsTo(Project::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(CategoryReport::class, 'category_id');
     }
 }
