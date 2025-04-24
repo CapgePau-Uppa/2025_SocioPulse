d8786125f15b70fc6c484a4d12a2c416eb4f7def
rexfollloys <rexfolloys@gmail.com>
2025-04-24 10:33:29 +0200

    favorite filter in landing page

--- front/src/app/landing-page/landing-page.component.ts
+++ front/src/app/landing-page/landing-page.component.ts
@@ -3,13 +3,16 @@ import { MatButton } from '@angular/material/button';
 import { RouterLink, Router } from '@angular/router';
 import { ProjectsService } from '../services/projects.service';
 import {MapComponent} from '../map/map.component';
+import { MatIcon } from '@angular/material/icon';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   imports: [
     MatButton,
     RouterLink,
-    MapComponent
+    MapComponent,
+    MatIcon
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
@@ -17,15 +20,42 @@ import {MapComponent} from '../map/map.component';
 
 export class LandingPageComponent implements OnInit {
   projects: any[] = [];
+  projects_all: any[] = [];
+  filteredByFavorite = false;
+  favoriteIds: number[] = [];
+
+  constructor(
+    private projectService: ProjectsService,
+    private router: Router,
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
-    console.log(sessionStorage.getItem('token'));
+    const token = sessionStorage.getItem('auth_token');
     this.projectService.getProjects().subscribe(data => {
+      this.projects_all = data;
       this.projects = data;
     });
+
+    if (token) {
+      const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+      this.http.get<any[]>(`http://localhost:8000/api/favorites`, { headers })
+        .subscribe(favs => {
+          this.favoriteIds = favs.map(f => f.project_id);
+        }, err => console.error(err));
+    }
   }
 
-  constructor(private projectService: ProjectsService, private router: Router) { }
+  toggleFavorite(): void {
+    this.filteredByFavorite = !this.filteredByFavorite;
+    if (this.filteredByFavorite) {
+      this.projects = this.projects_all.filter(p =>
+        this.favoriteIds.includes(p.id)
+      );
+    } else {
+      this.projects = [...this.projects_all];
+    }
+  }
 
   navigateToProjectDetails(projectId: number): void {
     this.router.navigate(['/project-detail-page', projectId]);
